733e1ac70cf19d3d3158adb44ba6e37edb435a1f
Vadim Galitsyn <vadim.galitsyn@oracle.com>
2026-03-03 13:28:18 +0000

    Linux: vboxdrv: Add support for UEK9 kernel on OL9 distro.
    svn:sync-xref-src-repo-rev: r172972

--- src/VBox/Installer/linux/vboxdrv.sh
+++ src/VBox/Installer/linux/vboxdrv.sh
@@ -104,6 +104,7 @@ KERN_VER=`uname -r`
 case "$KERN_VER" in
     5.15.0-*.el8uek*) PATH="/opt/rh/gcc-toolset-11/root/usr/bin:$PATH";;
     6.12.0-*.el9uek*)PATH="/opt/rh/gcc-toolset-14/root/usr/bin:$PATH";;
+    6.18.0-*.el9uek*)PATH="/opt/rh/gcc-toolset-14/root/usr/bin:$PATH";;
 esac
 
 if test -e "${MODULE_SRC}/vboxpci"; then
