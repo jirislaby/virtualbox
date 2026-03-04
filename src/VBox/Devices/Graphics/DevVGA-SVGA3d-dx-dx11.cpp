a40f33e439d83ee951c7b70ee982d5ce2aa00d31
Vitali Pelenjow <vitali.pelenjow@oracle.com>
2026-03-04 16:55:15 +0000

    Devices/Graphics: release decoder buffer on failure.
    svn:sync-xref-src-repo-rev: r173001

--- src/VBox/Devices/Graphics/DevVGA-SVGA3d-dx-dx11.cpp
+++ src/VBox/Devices/Graphics/DevVGA-SVGA3d-dx-dx11.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevVGA-SVGA3d-dx-dx11.cpp 113247 2026-03-04 12:12:24Z vitali.pelenjow@oracle.com $ */
+/* $Id: DevVGA-SVGA3d-dx-dx11.cpp 113254 2026-03-04 16:55:15Z vitali.pelenjow@oracle.com $ */
 /** @file
  * DevVMWare - VMWare SVGA device
  */
@@ -11771,7 +11771,14 @@ static DECLCALLBACK(int) vmsvga3dBackVBDXVideoDecoderSubmitBuffers(PVGASTATECC p
                                                         &DecoderBufferSize, &pDecoderBuffer);
         AssertReturnStmt(SUCCEEDED(hr), RTMemTmpFree(paDesc), VERR_NOT_SUPPORTED);
 
-        ASSERT_GUEST_CONTINUE(DecoderBufferSize >= s->dataSize);
+        if (DecoderBufferSize < s->dataSize)
+        {
+            ASSERT_GUEST_FAILED();
+            hr = pDXDevice->pVideoContext->ReleaseDecoderBuffer(pDXVideoDecoder->pVideoDecoder, d->BufferType);
+            AssertReturnStmt(SUCCEEDED(hr), RTMemTmpFree(paDesc), VERR_NOT_SUPPORTED);
+
+            continue;
+        }
 
         if (pSurface->pBackendSurface)
         {
